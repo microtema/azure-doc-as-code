e4d2b1576d580afd7e81d6ec0bfb1ba1d316f55b
microtema <microtema@web.de>
2023-10-05 15:59:32 +0200

    DX-merge templates

---
+++ src/main/java/de/microtema/docascode/update/MergeTemplates.java
@@ -0,0 +1,46 @@
+package de.microtema.docascode.update;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class MergeTemplates {
+
+    public static void main(String[] args) throws Exception {
+
+        File fromFolder = new File("./.docs"); //new File(args[0]);
+        File toFolder = new File("./docs"); //new File(args[1]);
+
+        System.out.println("Merge Templates from ./" + fromFolder.getName() + " to ./" + toFolder.getName());
+
+        FileUtils.deleteDirectory(toFolder);
+
+        for (File file : fromFolder.listFiles()) {
+
+            FileUtils.copyDirectoryToDirectory(file, toFolder);
+        }
+
+        for (File file : toFolder.listFiles()) {
+
+            String serviceName = file.getName();
+
+            renameFiles(file, serviceName);
+        }
+    }
+
+    private static void renameFiles(File fileOrFolder, String serviceName) {
+
+        if (fileOrFolder.isFile()) {
+
+            String fileName = fileOrFolder.getName();
+            String newFileName = fileName.replace(".md", String.format("[%s].md", serviceName));
+
+            fileOrFolder.renameTo(new File(fileOrFolder.getParentFile(), newFileName));
+        } else {
+
+            for (File file : fileOrFolder.listFiles()) {
+                renameFiles(file, serviceName);
+            }
+        }
+    }
+}
